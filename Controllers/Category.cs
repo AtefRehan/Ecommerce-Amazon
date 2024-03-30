42af81f15d02ea8be688e237418e01b893b139b4
dina ammam <dina.ammam34@gmail.com>
2024-03-31 01:20:46 +0200

    addGetAll-Category

--- Controllers/Category.cs
+++ Controllers/Category.cs
@@ -1,6 +1,34 @@
-﻿namespace ECommerce.Controllers
+﻿using AutoMapper;
+using ECommerce.DTOS.Category;
+using ECommerce.DTOS.Supplier;
+using ECommerce.Repositories.Catergory_Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
 {
-    public class Category
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Category:ControllerBase
     {
+        private readonly ICategoryRepository categoryRepo;
+        private readonly IMapper mapper;
+
+        public Category(ICategoryRepository _categoryRepo , IMapper _mapper)
+        {
+            categoryRepo = _categoryRepo;
+            mapper = _mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryReadDTO>>> GetAll()
+        {
+            List<CategoryReadDTO> categories = new List<CategoryReadDTO>();
+            foreach (var item in categoryRepo.GetAllCategories())
+            {
+                CategoryReadDTO c = mapper.Map<CategoryReadDTO>(item);
+                categories.Add(c);
+            }
+            return Ok(categories);
+        }
     }
 }
